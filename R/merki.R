7ac206ba71a1fb93908a277279b0cf586343f83d
Einar Hjorleifsson <einarhj@hafstokkur.hafro.is>
2016-11-05 14:31:13 +0000

    reinstalled taggart, fiskmerki_ functions not exported

--- R/merki.R
+++ R/merki.R
@@ -2,9 +2,9 @@
 #'
 #' @description XXX
 #'
-#'
 #' @param mar src_oracle tenging við oracle
-merki <- function(mar) {
+
+fiskmerki_merki <- function(mar) {
   d <- tbl_mar(mar, "fiskmerki.merki") %>%
     dplyr::rename(tid = id)
   return(d)
@@ -16,7 +16,7 @@ merki <- function(mar) {
 #'
 #'
 #' @param mar src_oracle tenging við oracle
-mfiskar <- function(mar) {
+fiskmerki_fiskar <- function(mar) {
 
   d <- tbl_mar(mar, "fiskmerki.fiskar") %>%
     dplyr::rename(fiskur_id = id)
@@ -30,20 +30,28 @@ mfiskar <- function(mar) {
 #'
 #'
 #' @param mar src_oracle tenging við oracle
-endurheimtur <- function(mar) {
+fiskmerki_endurheimtur <- function(mar) {
   d <- tbl_mar(mar, "fiskmerki.endurheimtur") %>%
-    dplyr::rename(rid = id,
+    dplyr::select(rid = id,
                   tid = merki_id,
-                  rtegund = tegund,
-                  rskip_nr = skip_nr,
-                  rreitur = reitur,
-                  rsmareitur = smareitur,
-                  rar = ar,
-                  rman = manudur,
-                  rlengd = lengd,
-                  rkyn = kyn,
-                  rkynthroski = kynthroski,
-                  rage = aldur)
+                  rDags = dags_fundid,
+                  rAr = ar,
+                  rManudur = manudur,
+                  rLon = vlengd,
+                  rLat = nbreidd,
+                  rReitur = reitur,
+                  rSmareitur = smareitur,
+                  rLandshluti = landshluti_id,
+                  rHafsvaedi = hafsvaedi_id,
+                  rVeidarfaeri = veidarfaeri_id,
+                  rDypi = dypi,
+                  rTegund = tegund,
+                  rLengd = lengd,
+                  rKyn = kyn,
+                  rKynthroski = kynthroski,
+                  rAge = aldur) %>%
+    mutate(lon = -geoconvert1(rLon * 100),
+           lat =  geoconvert1(rLat * 100))
 
   return(d)
 }
@@ -54,7 +62,7 @@ endurheimtur <- function(mar) {
 #'
 #'
 #' @param mar src_oracle tenging við oracle
-rafaudkenni <- function(mar) {
+fiskmerki_rafaudkenni <- function(mar) {
 
   d <- tbl_mar(mar, "fiskmerki.rafaudkenni") %>%
     dplyr::rename(tid = merki_id,
@@ -68,7 +76,7 @@ rafaudkenni <- function(mar) {
 #'
 #'
 #' @param mar src_oracle tenging við oracle
-dst <- function(mar) {
+fiskmerki_rafgogn <- function(mar) {
 
   d <- tbl_mar(mar, "fiskmerki.rafgogn") %>%
     dplyr::rename(dst_id = taudkenni)
@@ -81,16 +89,50 @@ dst <- function(mar) {
 #'
 #' @description A flat dataframe of tags and recaptures
 #'
+#' @export
 #'
 #' @param mar src_oracle tenging við oracle
 taggart <- function(mar) {
 
+  stodvar <-
+    lesa_stodvar(mar) %>%
+    mutate(tLon = -geoconvert1(kastad_v_lengd),
+           tLat =  geoconvert1(kastad_n_breidd)) %>%
+    select(synis_id,
+           leidangur,
+           stod,
+           tDags = dags,
+           tReitur = reitur,
+           tSmareitur = smareitur,
+           tLon,
+           tLat,
+           tDypi = dypi_kastad,
+           tVeidarfaeri = veidarfaeri)
+
+  fiskar <-
+    tbl_mar(mar, "fiskmerki.fiskar") %>%
+    select(fiskur_id = id,
+           synis_id,
+           tTegund = tegund,
+           tLengd = lengd,
+           tThyngd = thyngd,
+           tKyn = kyn,
+           tKynthroski = kynthroski)
+
+  merki <-
+    tbl_mar(mar, "fiskmerki.merki") %>%
+    select(tid = id,
+           fiskur_id,
+           audkenni,
+           numer)
+
   d <-
-    merki(mar) %>%
-    dplyr::left_join(mfiskar(mar),      by = "fiskur_id") %>%
-    dplyr::left_join(lesa_stodvar(mar), by = "synis_id") %>%
-    dplyr::left_join(endurheimtur(mar), by = "tid") %>%
-    dplyr::left_join(rafaudkenni(mar),  by = "tid")
+    fiskar %>%
+    dplyr::left_join(merki, by = "fiskur_id") %>%
+    dplyr::left_join(stodvar, by = "synis_id") %>%
+    dplyr::left_join(fiskmerki_endurheimtur(mar), by = "tid") %>%
+    dplyr::left_join(fiskmerki_rafaudkenni(mar),  by = "tid") %>%
+    select(-id, -tid)
 
   return(d)
 }
