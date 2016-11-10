b08aed881d9a52d406a544116899dfaad4899119
Einar Hjorleifsson <einarhj@hafstokkur.hafro.is>
2016-11-10 18:29:28 +0000

    seeded ata_***. no export, no helpfile

---
+++ R/ata.R
@@ -0,0 +1,127 @@
+
+ata_thurrvigt_tegund <- function(mar) {
+  tbl_mar(mar,"ata.thurrvigt_tegund") %>%
+    select(-c(snt:sbg))
+}
+
+ata_rummal_thattur <- function(mar) {
+  tbl_mar(mar,"ata.rummal_thattur") %>%
+    select(-c(snt:sbg))
+}
+
+ata_thurrvigt_thattur <- function(mar) {
+  tbl_mar(mar,"ata.thurrvigt_thattur") %>%
+    select(-c(snt:sbg))
+}
+
+ata_talningamadur <- function(mar) {
+  tbl_mar(mar,"ata.talningamadur") %>%
+    select(-c(snt:sbg))
+}
+
+ata_astand <- function(mar) {
+  tbl_mar(mar,"ata.astand") %>%
+    select(-c(snt:sbg))
+}
+
+ata_votvigt <- function(mar) {
+  tbl_mar(mar,"ata.votvigt") %>%
+    select(-c(snt:sbg))
+}
+
+ata_verkefni <- function(mar) {
+  tbl_mar(mar,"ata.verkefni") %>%
+    select(-c(snt:sbg))
+}
+
+ata_thurrvigt <- function(mar) {
+  tbl_mar(mar,"ata.thurrvigt") %>%
+    select(-c(snt:sbg))
+}
+
+ata_tegundir_hopar <- function(mar) {
+  tbl_mar(mar,"ata.tegundir_hopar") %>%
+    select(-c(snt:sbg))
+}
+
+ata_talning <- function(mar) {
+  tbl_mar(mar,"ata.talning") %>%
+    select(-c(snt:sbg))
+}
+
+ata_stod <- function(mar) {
+  tbl_mar(mar,"ata.stod") %>%
+    select(-c(snt:sbg))
+}
+
+ata_stig <- function(mar) {
+  tbl_mar(mar,"ata.stig") %>%
+    select(-c(snt:sbg))
+}
+
+ata_sofnunartaeki <- function(mar) {
+  tbl_mar(mar,"ata.sofnunartaeki") %>%
+    select(-c(snt:sbg))
+}
+
+ata_snid <- function(mar) {
+  tbl_mar(mar,"ata.snid") %>%
+    select(-c(snt:sbg))
+}
+
+ata_shortcut <- function(mar) {
+  tbl_mar(mar,"ata.shortcut") %>%
+    select(-c(snt:sbg))
+}
+
+ata_rummal <- function(mar) {
+  tbl_mar(mar,"ata.rummal") %>%
+    select(-c(snt:sbg))
+}
+
+ata_prosomlengd <- function(mar) {
+  tbl_mar(mar,"ata.prosomlengd") %>%
+    select(-c(snt:sbg))
+}
+
+ata_lengdir <- function(mar) {
+  tbl_mar(mar,"ata.lengdir") %>%
+    select(-c(sng:sbt))
+}
+
+ata_leidangur <- function(mar) {
+  tbl_mar(mar,"ata.leidangur") %>%
+    select(-c(snt:sbg))
+}
+
+ata_kynthroski <- function(mar) {
+  tbl_mar(mar,"ata.kynthroski") %>%
+    select(-c(snt:sbg))
+}
+
+ata_hopar_stig <- function(mar) {
+  tbl_mar(mar,"ata.hopar_stig") %>%
+    select(-c(sng:sbt))
+}
+
+ata_hopar <- function(mar) {
+  tbl_mar(mar,"ata.hopar") %>%
+    select(-c(snt:sbg))
+}
+
+ata_hafsyni <- function(mar) {
+  tbl_mar(mar,"ata.hafsyni") %>%
+    select(-c(snt:sbg))
+}
+ata_flurljomun <- function(mar) {
+  tbl_mar(mar,"ata.flurljomun") %>%
+    select(-c(snt:sbg))
+}
+ata_flurljomun <- function(mar) {
+  tbl_mar(mar,"ata.flurljomun") %>%
+    select(-c(snt:sbg))
+}
+ata_eggjaframleidsla <- function(mar) {
+  tbl_mar(mar,"ata.eggjaframleidsla") %>%
+    select(-c(snt:sbg))
+}
