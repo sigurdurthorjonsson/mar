acad8397870de1e810a85da8b41f0de62f318a17
Bjarki Þór Elvarsson <bthe@hafro.is>
2017-03-21 15:35:29 +0000

    Leiðréttingar á magafyrirspurnum eftir að *_tmp töflum var fleygt

--- R/magi.R
+++ R/magi.R
@@ -21,14 +21,15 @@ faeda_tegundir <- function(mar){
 #' @rdname faeda
 #' @export
 faeda_ranfiskar <- function(mar){
-  tbl_mar(mar,'faeda.f_fiskar_tmp') %>%
-    dplyr::filter(flokk_id > 1 &
-                    !(flokk_id %in% c(111845,111846,111847,111848,111849,111850,111851,111852,111853,
-                                      111854,111860,111861,111862,111863,111864,111869,111870,111871,
-                                      111938,111939,111940,111941,111942,111943,111944,111945,111946,
-                                      111947,111953,111954,111955,111956,111957,111977,111978,111979))) %>%
-    dplyr::anti_join(tbl_mar(mar,'faeda.f_fiskar') %>% select(flokk_id)) %>%
-    dplyr::union_all(tbl_mar(mar,'faeda.f_fiskar')) %>%
+  # tbl_mar(mar,'faeda.f_fiskar_tmp') %>%
+  #   dplyr::filter(flokk_id > 1 &
+  #                   !(flokk_id %in% c(111845,111846,111847,111848,111849,111850,111851,111852,111853,
+  #                                     111854,111860,111861,111862,111863,111864,111869,111870,111871,
+  #                                     111938,111939,111940,111941,111942,111943,111944,111945,111946,
+  #                                     111947,111953,111954,111955,111956,111957,111977,111978,111979))) %>%
+  #   dplyr::anti_join(tbl_mar(mar,'faeda.f_fiskar') %>% select(flokk_id)) %>%
+  #  dplyr::union_all(tbl_mar(mar,'faeda.f_fiskar')) %>%
+  tbl_mar(mar,'faeda.f_fiskar') %>%
     dplyr::mutate(len_fl =
                     if_else(lengd < 5,0,
                             if_else(lengd <7,1,
@@ -58,17 +59,19 @@ faeda_ranfiskar <- function(mar){
 #' @rdname faeda
 #' @export
 faeda_thyngdir <- function(mar){
-  tbl_mar(mar,'faeda.f_hopar_tmp') %>%
-    dplyr::filter(flokk_id > 1 &
-                    !(flokk_id %in% c(111845,111846,111847,111848,111849,111850,111851,111852,111853,
-                                      111854,111860,111861,111862,111863,111864,111869,111870,111871,
-                                      111938,111939,111940,111941,111942,111943,111944,111945,111946,
-                                      111947,111953,111954,111955,111956,111957,111977,111978,111979))) %>%
-    dplyr::mutate(uppruni = 'f_hopar_tmp') %>%
-    dplyr::anti_join(tbl_mar(mar,'faeda.f_hopar') %>%
-                       dplyr::select(flokk_id)) %>%
-    dplyr::union_all(tbl_mar(mar,'faeda.f_hopar') %>%
-                       dplyr::mutate(uppruni = 'f_hopar')) %>%
+  # tbl_mar(mar,'faeda.f_hopar_tmp') %>%
+  #   dplyr::filter(flokk_id > 1 &
+  #                   !(flokk_id %in% c(111845,111846,111847,111848,111849,111850,111851,111852,111853,
+  #                                     111854,111860,111861,111862,111863,111864,111869,111870,111871,
+  #                                     111938,111939,111940,111941,111942,111943,111944,111945,111946,
+  #                                     111947,111953,111954,111955,111956,111957,111977,111978,111979))) %>%
+  #   dplyr::mutate(uppruni = 'f_hopar_tmp') %>%
+  #   dplyr::anti_join(tbl_mar(mar,'faeda.f_hopar') %>%
+  #                      dplyr::select(flokk_id)) %>%
+  #  dplyr::union_all(tbl_mar(mar,'faeda.f_hopar') %>%
+  #                     dplyr::mutate(uppruni = 'f_hopar')) %>%
+  tbl_mar(mar,'faeda.f_hopar') %>%
+    dplyr::mutate(uppruni = 'f_hopar') %>%
     dplyr::mutate(thyngd = ifelse(thyngd==-1,0.2,
                                   ifelse(thyngd<0,0,thyngd)),
                   faedutegund = faeduhopur,
@@ -91,20 +94,20 @@ faeda_lengdir <- function(mar){
            uppruni = 'f_lengdir') %>%
     dplyr::select(synis_id, faerslunumer, flokk_id, ranfiskur, faeduhopur,
                   brad_kyn, lengd, fjoldi,  haed, lengd_rf,len_fl,uppruni) %>%
-    dplyr::union_all(tbl_mar(mar,'faeda.f_staerdir_tmp') %>%
-                       dplyr::filter(flokk_id > 1 &
-                                       !(synis_id %in% c(137113,137123,137129,241142))) %>%
-                       dplyr::anti_join(tbl_mar(mar,'faeda.f_staerdir') %>%
-                                          dplyr::select(synis_id)) %>%
-                       dplyr::union_all(tbl_mar(mar,'faeda.f_staerdir')) %>%
-                       dplyr::mutate(lengd = lengd/10,
-                                     brad_kyn = ifelse(faeduhopur=='mall vil',brad_kyn + 1,
-                                                       brad_kyn),
-                                     lengd_rf = 1*lengd_rf,
-                                     len_fl = NA,
-                                     uppruni = 'f_staerdir') %>%
-                       dplyr::select(synis_id, faerslunumer, flokk_id, ranfiskur, faeduhopur,
-                                     brad_kyn, lengd, fjoldi,  haed, lengd_rf,len_fl,uppruni)) %>%
+#    dplyr::union_all(tbl_mar(mar,'faeda.f_staerdir_tmp') %>%
+#                       dplyr::filter(flokk_id > 1 &
+#                                       !(synis_id %in% c(137113,137123,137129,241142))) %>%
+#                       dplyr::anti_join(tbl_mar(mar,'faeda.f_staerdir') %>%
+#                                          dplyr::select(synis_id)) %>%
+#                       dplyr::union_all(tbl_mar(mar,'faeda.f_staerdir')) %>%
+#                       dplyr::mutate(lengd = lengd/10,
+#                                     brad_kyn = ifelse(faeduhopur=='mall vil',brad_kyn + 1,
+#                                                       brad_kyn),
+#                                     lengd_rf = 1*lengd_rf,
+#                                     len_fl = NA,
+#                                     uppruni = 'f_staerdir') %>%
+#                       dplyr::select(synis_id, faerslunumer, flokk_id, ranfiskur, faeduhopur,
+#                                     brad_kyn, lengd, fjoldi,  haed, lengd_rf,len_fl,uppruni)) %>%
     dplyr::mutate(brad_kynth = NA) %>%
     dplyr::union_all(tbl_mar(mar,'faeda.f_kynthroski') %>%
                        dplyr::mutate(fjoldi = 1,
