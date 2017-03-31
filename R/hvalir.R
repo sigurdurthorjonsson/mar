7650af9a2332e8e61237745f130f7c9c2b0c549f
Bjarki Þór Elvarsson <bthe@hafro.is>
2017-03-31 09:48:08 +0000

    snurfuss fyrir hvala-fyrirspurn

--- R/hvalir.R
+++ R/hvalir.R
@@ -7,6 +7,11 @@
 #'
 #' @examples
 hvalir_hvalir <- function(mar){
-  tbl_mar(mar,'hvalir.hvalir_v')
+  tbl_mar(mar,'hvalir.hvalir_v') %>%
+    dplyr::mutate(veiddur_breidd = to_number(veiddur_breidd),
+                  veiddur_lengd = to_number(veiddur_lengd)) %>%
+    dplyr::select_(.dots = colnames(tbl_mar(mar,"hvalir.hvalir_v"))) %>%
+    dplyr::mutate(ar = to_char(dags_veidi,'yyyy'),
+                  er_fostur = ifelse(substr(radnumer,-1)=='F',1,0))
 }
 
