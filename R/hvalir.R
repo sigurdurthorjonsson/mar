42c911589c1150e5b24f0d725e4097cabf75e508
Bjarki Þór Elvarsson <bthe@hafro.is>
2017-10-18 15:48:39 +0000

    minniháttar fix á hvalir_hvalir þar sem tékkað er á kommum í staðsetningu og skipt út fyrir punkt

--- R/hvalir.R
+++ R/hvalir.R
@@ -8,8 +8,8 @@
 #' @examples
 hvalir_hvalir <- function(mar){
   tbl_mar(mar,'hvalir.hvalir_v') %>%
-    dplyr::mutate(veiddur_breidd = to_number(veiddur_breidd),
-                  veiddur_lengd = to_number(veiddur_lengd)) %>%
+    dplyr::mutate(veiddur_breidd = to_number(replace(veiddur_breidd,',','.')),
+                  veiddur_lengd = to_number(replace(veiddur_lengd,',','.'))) %>%
     dplyr::select_(.dots = colnames(tbl_mar(mar,"hvalir.hvalir_v"))) %>%
     dplyr::mutate(ar = to_char(dags_veidi,'yyyy'),
                   er_fostur = ifelse(substr(radnumer,-1,0)=='F',1,0))
