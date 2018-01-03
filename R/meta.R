9adab1ce70b451bd2c24aea119fa466544bb8108
Bjarki Þór Elvarsson <bthe@hafro.is>
2018-01-03 10:54:33 +0000

    Fall til þess að lesa sjávardýraorðabókina

--- R/meta.R
+++ R/meta.R
@@ -50,3 +50,15 @@ kvoti_stadur <- function(mar) {
 
 }
 
+#' Orðabók
+#'
+#' Flettir upp í sjávardýraorðbókinni á Hafró vefnum
+#' @param mar
+#'
+#' @return db query object
+#' @export
+ordabok <- function(mar){
+  tbl_mar(mar, "orri.ordabok") %>%
+    dplyr::left_join(tbl_mar(mar, "orri.ordabok_mal")) %>%
+    dplyr::select(tegund = nafn,erlent_heiti = heiti,tungumal=nafn_mals,skammst)
+}
