c805f53ffce0cfb1845b961c1f27cb50d4fa439d
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-07-12 12:17:42 +0000

    hydro_station betrumbætt

--- R/hydro.R
+++ R/hydro.R
@@ -26,8 +26,14 @@ hydro_trolltog <- function(mar) {
     select(-c(snt:sbn))
 }
 #' @export
-hydro_station <- function(mar) {
-  tbl_mar(mar, "hydro.station") %>%
-    select(-c(snt:sbn))
+hydro_station <- function(con) {
+  tbl_mar(con, "hydro.station") %>%
+    select(-c(snt:sbn)) %>%
+    mutate(lon =   as.integer(longitude) * 100 + ifelse(is.na(la_sec), 0, as.integer(lo_sec)),
+           lat =   as.integer(latitude)  * 100 + ifelse(is.na(lo_sec), 0, as.integer(la_sec))) %>%
+    mar:::geoconvert(col.names = c("lon", "lat")) %>%
+    mutate(lon = ifelse(lo_id == "W", -lon,  lon),
+           lat = ifelse(la_id == "N",  lat, -lat)) %>%
+    select(t_id:id, lon, lat, q_cont:name)
 }
 
