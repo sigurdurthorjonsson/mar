d937d99c1244e09765f3ed311c3c710febefac10
Bjarki Þór Elvarsson <bthe@hafogvatn.is>
2020-04-07 10:13:21 +0000

    An attempt to fish out even more location data in the logbooks..

--- R/fiskar.R
+++ R/fiskar.R
@@ -18,7 +18,9 @@ lesa_stodvar <- function(mar) {
                                (kastad_v_lengd + hift_v_lengd) / 2),
                   lat = ifelse(is.na(hift_n_breidd),
                                kastad_n_breidd,
-                               (kastad_n_breidd + hift_n_breidd) / 2))
+                               (kastad_n_breidd + hift_n_breidd) / 2)) %>%
+    dplyr::mutate(reitur = nvl(reitur, d2r(lat,lon)),
+                  smareitur = nvl(smareitur,d2sr(lat,lon)-10*d2r(lat,lon)))
 }
 
 
