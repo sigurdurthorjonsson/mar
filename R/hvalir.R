a0a1debee744769f715baa49379d144f3978b772
Bjarki Þór Elvarsson <bthe@hafro.is>
2020-06-19 10:26:31 +0000

    fæðuföll flutt út

--- R/hvalir.R
+++ R/hvalir.R
@@ -10,8 +10,8 @@
 #'
 hvalir_hvalir <- function(con) {
   tbl_mar(con, 'hvalir.hvalir_v') %>%
-    dplyr::mutate(veiddur_breidd = to_number(replace(veiddur_breidd,',','.')),
-                  veiddur_lengd = to_number(replace(veiddur_lengd,',','.'))) %>%
+    dplyr::mutate(veiddur_breidd = to_number(replace(nvl(veiddur_breidd,0),',','.')),
+                  veiddur_lengd = to_number(replace(decode(veiddur_lengd,'-',NULL,veiddur_lengd),',','.'))) %>%
     dplyr::select_(.dots = colnames(tbl_mar(mar,"hvalir.hvalir_v"))) %>%
     dplyr::mutate(ar = to_char(dags_veidi,'yyyy'),
                   er_fostur = ifelse(substr(radnumer,-1,0)=='F',1,0))
