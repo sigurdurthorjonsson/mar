91026ef2e996e9ca2c55dc3a3984ee39c065d167
Sigurður Þór Jónsson <sigurdur@hafro.is>
2021-04-30 10:16:39 +0000

    Update connect_mar.R
    borrowing the connection function from dev/mar, adapting to the new MFRI-db

--- R/connect_mar.R
+++ R/connect_mar.R
@@ -1,13 +1,14 @@
 
 #' Connect to MFRI database
 #'
-#' A shortcut method
+#' A shortcut method, borrowing from gitlab.hafogvatn.is/dev/mar
 #'
+#' @param dbname Name of DB to connect to
 #' @param ... Additional arguements to pass to DBI functions
 #'
 #' @return A connection to the Oracle database
 #' @export
 
-connect_mar <- function(...) {
-  DBI::dbConnect(DBI::dbDriver("Oracle"), ...)
+connect_mar <- function(dbname='sjor', ...) {
+  ROracle::dbConnect(DBI::dbDriver("Oracle"), dbname=dbname, ...)
 }
